296167b4551805955671f24fa8db25e990e4848f
Vincent <vwierenga@gmail.com>
2016-10-12 16:58:13 +0200

    Added security checks to ratings. Password is hidden when requisting a list of users.

--- app.js
+++ app.js
@@ -62,9 +62,15 @@ router.route('/authenticate')
 
                     // if user is found and password is right
                     // create a token
-                    var token = jwt.sign(user, app.get('secret'), {
+                    var payload = {
+                        username: user.username,
+                    };
+
+                    var token = jwt.sign(payload, app.get('secret'), {
                         expiresIn: 86400 // expires in 24 hours
                     });
+
+                    //console.log(jwt.decode(token))
 
                     // return the information including token as JSON
                     res.json({
@@ -94,10 +100,26 @@ router.route('/movies/:imdb_number')
 
     //Get the movie with a specific imdb id http://localhost:8080/api/movies/:imdb_number
     .get(function(req, res) {
-        Movie.findById(req.params.imdb_number, function(err, movie) {
+        Movie.find({'imdb_number':req.params.imdb_number}, function(err, movie) {
             if (err)
                 res.send(err);
             res.json(movie);
+        });
+    });
+
+router.route('/movieswithrating') //Not finished yet
+
+//Get the movie with a specific imdb id http://localhost:8080/api/movies/:imdb_number
+    .get(function(req, res) {
+        Movie.find(function(err, movies) {
+            if (err)
+                res.send(err);
+            Rating.find({}, '', function(err, ratings) {
+                if (err)
+                    res.send(err);
+                res.json(ratings);
+            });
+            res.json(movies);
         });
     });
 
@@ -158,7 +180,7 @@ router.route('/users')
 
 //Get all users http://localhost:8080/api/users
     .get(function(req, res) {
-        User.find(function(err, users) {
+        User.find({}, 'first_name surname_prefix last_name username ', function(err, users) {
             if (err)
                 res.send(err);
             res.json(users);
@@ -169,7 +191,7 @@ router.route('/users/:user_id')
 
 //Get the user with a specific id http://localhost:8080/api/movies/:user_id
     .get(function(req, res) {
-        User.findById(req.params.user_id, function(err, user) {
+        User.findById(req.params.user_id, 'first_name surname_prefix last_name username ', function(err, user) {
             if (err)
                 res.send(err);
             res.json(user);
@@ -180,26 +202,35 @@ router.route('/users/:user_id')
 //-------------------------------------------------------------------------------------------------------------
 router.route('/ratings')
 
-//Create a user with POST http://localhost:8080/api/ratings)
+//Create a rating with POST http://localhost:8080/api/ratings)
     .post(function(req, res) {
 
-        var rating = new Rating();
-        rating.imdb_number = req.body.imdb_number;
-        rating.rating = req.body.rating;
-        rating.by_user = req.body.by_user;
+        var score = req.body.rating;
 
-        //Save the user and check for errors
-        rating.save(function(err) {
-            if (err)
-                res.send(err);
+        if(score >= 1 && score <= 10){ //Rating from 1 to 10, 1 = 0.5 stars, 10 = 5 stars
+            var rating = new Rating();
+            rating.imdb_number = req.body.imdb_number;
+            rating.rating = req.body.rating;
+            rating.by_user = req.body.by_user;
 
-            res.json({ message: 'Rating created!' });
-        });
+            //Save the user and check for errors
+            rating.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Rating created!' });
+            });
+        } else {
+            res.json({ message: 'The score should be between 1 and 10 inclusive' });
+        }
     })
 
     //Get all ratings http://localhost:8080/api/ratings
     .get(function(req, res) {
-        Rating.find(function(err, ratings) {
+
+        var current_user = req.decoded.username;
+
+        Rating.find({'by_user':current_user},function(err, ratings) {
             if (err)
                 res.send(err);
             res.json(ratings);
@@ -210,46 +241,61 @@ router.route('/ratings/:rating_id')
 
 //Get the user with a specific id http://localhost:8080/api/ratings/:rating_id
     .get(function(req, res) {
+
+        var current_user = req.decoded.username;
+
         Rating.findById(req.params.rating_id, function(err, rating) {
-            if (err)
-                res.send(err);
-            res.json(rating);
+            if(rating.by_user == current_user) {
+                if (err)
+                    res.send(err);
+                res.json(rating);
+            } else {
+                res.json({ message: 'You do not have the neccesary permissions to view this rating! :(' });
+            }
         });
     })
 
     //Update the rating http://localhost:8080/api/ratings/:rating_id)
     .put(function(req, res) {
 
+        var current_user = req.decoded.username;
+
         Rating.findById(req.params.rating_id, function(err, user) {
-
-            if (err)
-                res.send(err);
-
-            rating.imdb_number = req.body.imdb_number;
-            rating.rating = req.body.rating;
-            rating.by_user = req.body.by_user;
-
-            // save the rating
-            user.save(function(err) {
+            if(rating.by_user == current_user) {
                 if (err)
                     res.send(err);
 
-                res.json({ message: 'Rating updated!' });
-            });
+                rating.imdb_number = req.body.imdb_number;
+                rating.rating = req.body.rating;
+                rating.by_user = req.body.by_user;
 
+                // save the rating
+                user.save(function(err) {
+                    if (err)
+                        res.send(err);
+
+                    res.json({ message: 'Rating updated!' });
+                });
+            } else {
+                res.json({ message: 'You do not have the neccesary permissions to modify this rating! :(' });
+            }
         });
     })
 
     //Delete the rating http://localhost:8080/api/ratings/:rating_id)
     .delete(function(req, res) {
-        Rating.remove({
-            _id: req.params.rating_id
-        }, function(err, rating) {
-            if (err)
-                res.send(err);
+        if(rating.by_user == current_user) {
+            Rating.remove({
+                _id: req.params.rating_id
+            }, function(err, rating) {
+                if (err)
+                    res.send(err);
 
-            res.json({ message: 'Rating deleted!' });
-        });
+                res.json({ message: 'Rating deleted!' });
+            });
+        } else {
+            res.json({ message: 'You do not have the neccesary permissions to delete this rating! :(' });
+        }
     });
 
 
