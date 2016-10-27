cee8708652f4e830852cc1453f4eb35c640f638d
Rick Harmelink <rimihaeg@live.nl>
2016-10-27 14:25:21 +0200

    standard login/register page + bootstrap

---
+++ website/js/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Rick on 27-10-2016.
+ */
+
+function showRegister(){
+    document.getElementById('registerDiv').style.display = 'block';
+    document.getElementById('loginButtons').style.display = 'none';
+}
+
+function hideRegister(){
+
+}
+
+function login(){
+    console.log('login functie');
+}
+
+function register(){
+    console.log('register functie')
+}
